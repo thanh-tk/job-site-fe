94f3550c629dabb5ff0d1fcaf2cca634b1612ea6
thanh-tk <thaikimthanh96@gmail.com>
2023-11-02 12:52:56 +0700

    handle API , show message on error

--- src/services/jobs/index.tsx
+++ src/services/jobs/index.tsx
@@ -1,12 +1,19 @@
 import { JobData, QueryDataResponse, QueryParam } from "interfaces";
 import { BaseService } from "services/base";
 
-
 export default class JobServices extends BaseService<JobData> {
   constructor() {
     super(`${process.env.REACT_APP_API_URL}/job`);
   }
-  async fetchJobsList(query: QueryParam): Promise<QueryDataResponse<JobData[]>> {
-    return await this.getList(`/jobs`, query);
+  async fetchJobsList(
+    query: QueryParam
+  ): Promise<QueryDataResponse<JobData[]>> {
+    return this.getList(`/jobs`, query)
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        throw error;
+      });
   }
 }
